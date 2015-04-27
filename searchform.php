78f441227dfe3c0e1441e64ae6ced17a34060b74
webitbuzz <mofassel.me@gmail.com>
2015-04-27 18:04:59 +0600

    searchform.php finally added

---
+++ searchform.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This custom search form is used by search pages.
+ *
+ * @package Boot WP Framework
+ */
+ ?>
+<form action="http://localhost/bootwp/" class="search-form form-inline" method="get" role="search">
+	<div class="form-group">
+		<span class="screen-reader-text"> Search for:</span>
+		<input type="search" class="form-control" title="Search for:" name="s" value="" placeholder="Search …" class="search-field">
+	</div>
+	<button type="submit" class="search-submit btn btn-default">Search</button>
+</form>
